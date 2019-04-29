acce6b473c9908d0e7da66c2bbc3b1e165bb5391
unknown <berkelmas96@gmail.com>
2019-04-29 21:49:37 +0300

    deneme console log

--- src/actions/loginAction.js
+++ src/actions/loginAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export function usernameAction(data) { 
+export function usernameAction(data) {
     return {
         type: 'username_action',
         payload: {
@@ -53,7 +53,9 @@ export function submitAction(){
             password
         })
         .then((response) => {
+            console.log(response.request.response);
             const res = JSON.parse(response.request.response);
+
             if (res.status) {
                 localStorage.setItem('jwttoken', res.token);
                 dispatch(loginSuccess())
@@ -72,15 +74,15 @@ export function checkLogin() {
         const token = localStorage.getItem('jwttoken');
         const {domain} = getState().userReducer;
         if (token) {
-            // Burada axios ile headerımıza token'ımızı koyup bir get request alarak 
+            // Burada axios ile headerımıza token'ımızı koyup bir get request alarak
             // serverdan acaba tokenımızın onay alıp almayacagına bakacagız.
 
-            const AuthStr = 'Bearer '.concat(token); 
+            const AuthStr = 'Bearer '.concat(token);
             axios.get(domain + 'getir/reactonay', {headers : { Authorization : AuthStr }})
                 .then(response => {
                         if (response.data.status === 200) {
                             dispatch(loginSuccess());
-                        } 
+                        }
                     })
                 .catch(err => console.log(err))
 
